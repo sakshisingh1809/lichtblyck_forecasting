81350218e6f2485d6023d58a9ea98ff0ef2f2692
Sakshi Singh <sakshi.singh@lichtblick.de>
2021-11-24 12:31:52 +0100

    Static visualisation of various pfstates

--- lichtblyck/core/output_plot.py
+++ lichtblyck/core/output_plot.py
@@ -3,6 +3,8 @@ Module with mixins, to add 'plot-functionality' to PfLine and PfState classes.
 """
 
 from __future__ import annotations
+
+import matplotlib
 from ..visualize import visualize as vis
 from ..tools import nits
 from typing import Dict, TYPE_CHECKING
@@ -79,13 +81,26 @@ class PfStatePlotOutput:
             price `p` [Eur/MWh].
         Any additional kwargs are passed to the pd.Series.plot function.
         """
-        if not col:
-            col = "w" if "w" in self.available else "p"
-        how = {"r": "bar", "q": "bar", "p": "step", "w": "line"}.get(col)
-        if not how:
-            raise ValueError(f"`col` must be one of {', '.join(self.available)}.")
-        s = self[col]
-        vis.plot_timeseries(ax, s, how=how, **kwargs)
+        if line == "offtake":
+            (-self.offtake).plot_to_ax(ax, col)
+            ax.bar_label(
+                ax.containers[0], label_type="edge", fmt="%.0f"
+            )  # print labels on top of each bar
+
+        elif line == "hedgedfraction":
+            hedgefraction = -self.sourced.volume / self.offtake.volume
+            vis.plot_timeseries_as_bar(ax, hedgefraction, color="grey")
+            ax.bar_label(
+                ax.containers[0],
+                label_type="edge",
+                labels=[f"{val:.0%}" for val in hedgefraction],
+            )  # print labels on top of each bar
+
+        elif line == "price":
+            vis.plot_timeseries_as_bar(ax, self.unsourcedprice["p"], alpha=0.0)
+            ax.bar_label(
+                ax.containers[0], label_type="center"
+            )  # print labels on top of each bar
 
     def plot(self: PfState, cols: str = "wp") -> plt.Figure:
         """Plot one or more timeseries of the portfolio state.
@@ -112,25 +127,19 @@ class PfStatePlotOutput:
         )  # make x-axis tick labels on the top of a plot
         ax1.xaxis.set_tick_params(labelbottom=False)
         ax1.set_title("Offtake Volume")
-        (-self.offtake).plot_to_ax(ax1, "q")
-
-        ax1.bar_label(
-            ax1.containers[0], label_type="edge", fmt="%.0f"
-        )  # print labels on top of each bar
+        self.plot_to_ax(ax1, "offtake", "q")
 
         # plot Hedged volumne (%)
         ax2 = plt.subplot2grid(shape=(2, 2), loc=(0, 1), colspan=1)
-        hedgefraction = -self.sourced.volume / self.offtake.volume
+
         ax2.set_title("Hedged Fraction")
         ax2.set_ylabel("Percentage")
         ax2.xaxis.set_tick_params(
             labeltop=True
         )  # make x-axis tick labels on the top of a plot
         ax2.xaxis.set_tick_params(labelbottom=False)
-        vis.plot_timeseries_as_bar(ax2, hedgefraction * 100, color="grey")
-        ax2.bar_label(
-            ax2.containers[0], label_type="edge", fmt="%.0f",
-        )  # print labels on top of each bar
+        ax2.yaxis.set_major_formatter(matplotlib.ticker.PercentFormatter(1.0))
+        self.plot_to_ax(ax2, "hedgedfraction")
 
         # plot price
         ax3 = plt.subplot2grid(shape=(2, 2), loc=(1, 0), colspan=1)
@@ -141,47 +150,58 @@ class PfStatePlotOutput:
         ax3.set_title("Portfolio Price")
         ax3.axes.get_xaxis().set_visible(False)
         plt.yticks(color="w")
-        vis.plot_timeseries_as_bar(ax3, self.unsourcedprice["p"], alpha=0.0)
-        ax3.bar_label(
-            ax3.containers[0], label_type="center"
-        )  # print labels on top of each bar
+        self.plot_to_ax(ax3, "price")
 
 
-def plot_pfstates(dic: Dict[str, PfState]) -> plt.Figure:
-    """Plot multiple PfState instances.
+def plot_pfstates(dic: Dict[str, PfState], freq: str = "MS") -> plt.Figure:
+    """Plot multiple PfState instances. 
 
     Parameters
     ----------
     dic : Dict[str, PfState]
         Dictionary with PfState instances as values, and their names as the keys.
+        The Dictionary argument is a dictionary with pretty portfolio names as keys,
+        and the PfState instances as values
 
     Returns
     -------
     plt.Figure
         The figure object to which the instances were plotted.
     """
+    fig = plt.figure()
+    fig.set_size_inches(20, 10)
 
-    fig, ax = plt.subplots(
-        len(Dict), 1, True, False, squeeze=False, figsize=(10, len(Dict) * 3)
-    )
-    # make x-axis tick labels on the top of a plot
-    plt.rcParams["xtick.bottom"] = plt.rcParams["xtick.labelbottom"] = False
-    plt.rcParams["xtick.top"] = plt.rcParams["xtick.labeltop"] = True
+    for i, (pfname, pfs) in enumerate(dic.items()):
+        pfs = pfs.changefreq(freq)
+        ax1 = plt.subplot2grid(shape=(len(dic) + 4, 2), loc=(0 + i * 2, 0), colspan=1)
+        ax1.xaxis.set_tick_params(
+            labeltop=True
+        )  # make x-axis tick labels on the top of a plot
+        ax1.xaxis.set_tick_params(labelbottom=False)
+        ax1.set_title("Offtake Volume")
+        pfs.plot_to_ax(ax1, "offtake", "q")
 
-    # plot for offtake [GWh]
-    if Dict.keys() == "offtakevolume":
-        ax.set_xlabel("GWh")
-        ax.set_title("Offtake [GWh]")
-        ax.bar(Dict.values(), color=vis.Colors.Wqpr)
-        for index, data in enumerate(Dict.values()):
-            plt.text(x=index, y=data + 1, s=f"{data}", fontdict=dict(fontsize=20))
+        ax2 = plt.subplot2grid(shape=(len(dic) + 4, 2), loc=(0 + i * 2, 1), colspan=1)
+        hedgefraction = -pfs.sourced.volume / pfs.offtake.volume
+        ax2.set_title("Hedged Fraction")
+        ax2.set_ylabel("Percentage")
+        ax2.xaxis.set_tick_params(
+            labeltop=True
+        )  # make x-axis tick labels on the top of a plot
+        ax2.xaxis.set_tick_params(labelbottom=False)
+        pfs.plot_to_ax(ax2, "hedgedfraction")
 
-    elif Dict.keys() == "sourced":
-        ax.set_title("Hedged [%]")
+        ax3 = plt.subplot2grid(shape=(len(dic) + 4, 2), loc=(1 + i * 2, 0), colspan=1)
+        ax3.set_frame_on(False)
+        plt.ylim(-1000000, 1000000)
+        ax3.set_yticklabels([])  # make yticks disappear
+        ax3.get_xaxis().tick_bottom()
+        ax3.set_title("Portfolio Price")
+        ax3.axes.get_xaxis().set_visible(False)
+        plt.yticks(color="w")
+        pfs.plot_to_ax(ax3, "price")
 
-    elif Dict.keys() == "unsourcedprice":
-        ax.set_xlabel("Euro/MWh")
-        ax.set_title("Unhedged")
+        # draw 2d line after one portfolio
+        # line = plt.Line2D([0, 1], [y, y], transform=fig.transFigure, color="black")
+        # fig.add_artist(line)
 
-    plt.tight_layout()
-    plt.show()
