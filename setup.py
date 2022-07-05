ad99f6ecac72094907a7d9b02465da2a2b4d9d85
Ruud Wijtvliet <ruud.wijtvliet@lichtblick.de>
2022-07-05 18:16:10 +0200

    changed version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="Lichtblyck",
-    version="0.1dev",
+    version="0.1.dev1",
     packages=find_packages(exclude=["*tests.*", "*tests"]),
     install_requires=[line.strip() for line in open("requirements.txt", "r")],
     python_requires=">=3.8",
