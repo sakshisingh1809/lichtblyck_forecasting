a20794c98096b81f10d30354b9fc69e7575c6ac5
Ruud Wijtvliet <ruud.wijtvliet@lichtblick.de>
2022-07-05 18:13:12 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -4,9 +4,7 @@ from setuptools import find_packages
 setup(
     name="Lichtblyck",
     version="0.1dev",
-    packages=[
-        "lichtblyck",
-    ],
+    packages=find_packages(exclude=["*tests.*", "*tests"]),
     install_requires=[line.strip() for line in open("requirements.txt", "r")],
     python_requires=">=3.8",
     include_package_data=True,
