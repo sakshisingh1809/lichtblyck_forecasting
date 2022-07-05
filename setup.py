489e3942c8e03b3650fcb6d9ee81c619b99eaadb
Ruud Wijtvliet <ruud.wijtvliet@lichtblick.de>
2022-07-05 18:06:08 +0200

    bugfixing pipeline

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
-    name='Lichtblyck',
-    version='0.1dev',
-    packages=['lichtblyck',],
-    long_description=open('README.md').read(),
+    name="Lichtblyck",
+    version="0.1dev",
+    packages=[
+        "lichtblyck",
+    ],
+    install_requires=[line.strip() for line in open("requirements.txt", "r")],
+    python_requires=">=3.8",
+    include_package_data=True,
+    long_description=open("README.md").read(),
 )
