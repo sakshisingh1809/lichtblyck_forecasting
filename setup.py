d281c06dfec3dc967fe91b41ec05fdfe074e2065
Robert Brown <robert.brown@lichtblick.de>
2022-07-15 08:51:16 +0000

    Merged PR 13661: DATA-1829: Minor packing updates to fix import errors
    small packing changes to fix import errors

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from setuptools import setup
 from setuptools import find_packages
 
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+# https://github.com/scikit-learn/scikit-learn/blob/sklearn/setup.py
+
+#   X.Y.0   # For first release after an increment in Y
+#   X.Y.Z   # For bugfix releases
+
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+
 setup(
     name="Lichtblyck",
-    version="0.1.dev1",
-    packages=find_packages(exclude=["*tests.*", "*tests"]),
+    version="0.1.dev02",
+    zip_safe=False,
+    packages=find_packages(exclude=["tests"]),
     install_requires=[line.strip() for line in open("requirements.txt", "r")],
     python_requires=">=3.8",
     include_package_data=True,
